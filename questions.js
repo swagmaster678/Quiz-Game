15ccc4678b63090010d7b99d214fed53219e0573
Brice Whitefield <bricewhitefield@yahoo.com>
2021-03-18 18:54:39 -0500

    added questions

---
+++ questions.js
@@ -0,0 +1,71 @@
+let quizQuestion = [
+    {
+        question: "What is a Boolean?",
+        answers: {
+            A: "A binary variable, having two possible values called “true” and “false.”.",
+            
+            B: "A funny word",
+
+            C: "A standalone entity that holds multiple values in terms of properties and methods.",
+
+            D: "An array is an ordered list of values",
+        },
+        correctAnswer: "A"
+    },
+    {
+        question: "What is the correct way to code an array?",
+        answers: {
+                A: "var cars = ['Saab' | 'Volvo' | BMW];",
+                
+                B: "var cars = {'Saab:Volvo:BMW};",
+
+                C: "var cars = ['Saab', 'Volvo', 'BMW'];",
+
+                D: "var cars = ['Saab', 'Volvo', 'BMW']",
+            },
+            correctAnswer: "C"
+    },
+    {
+        question: "How do you add an item to LocalStorage?",
+        answers: {
+                A:"localStorage.setItem('mycat , 'Tom')",
+
+                B:"virtually places item in localStorage",
+
+                C:"localStorage.getitem('mycat, 'Tom')",
+
+                D:"localStorage.item('mycat, 'Tom')",
+        },
+            correctAnswer: "A"
+
+        },
+        {
+        question: "What is the correct way to call the random method on the Math global object?",
+        answers: {
+                A:"random.Math()",
+
+                B:"Math.random()", 
+
+                C:"Math(random)",  
+
+                D:"math.random()",
+        },
+            correctAnswer: "B"
+
+        },
+        {
+        question: "What is the correct way to declare a new variable that you can change?",
+        answers: {
+                A:"let myName = 'Brice'",
+
+                B:"myName = 'Brice'",
+
+                C:"let myName: 'Sloan'",
+
+                D:"const myName = 'Brice'",
+        },
+            correctAnswer: "A"
+
+        },
+        
+];
